e9a865677b5d511fc1d435c48e72a1c66e8bf339
eugeniothiago <thiagoace1@hotmail.com>
2022-09-18 23:44:18 -0300

    added html output type

--- bionic_reader.py
+++ bionic_reader.py
@@ -7,10 +7,14 @@ def bionic_reader(text: str, text_type: str) -> str:
         ansi_bold = "\033[1m"
         ansi_end = "\033[0m"
         markdown_bold = "**"
+        html_bold = "<b>"
+        html_bold_end = "</b>"
         if text_type == "ansi":
             return f"{ansi_bold}{word[:idx]}{ansi_end}{word[idx:]}"
         elif text_type == "markdown":
             return f"{markdown_bold}{word[:idx]}{markdown_bold}{word[idx:]}"
+        elif text_type == 'html':
+            return f"{html_bold}{word[:idx]}{html_bold_end}{word[idx:]}"
 
     bolded_text = []
     for word in text.split():
