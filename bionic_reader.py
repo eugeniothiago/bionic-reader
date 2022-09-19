b78e9966140d80da390bdbfc7b2d6182f5cb8047
eugeniothiago <thiagoace1@hotmail.com>
2022-09-18 23:37:28 -0300

    added markdown bold option

--- bionic_reader.py
+++ bionic_reader.py
@@ -2,18 +2,22 @@ import os
 from math import floor
 
 
-def bionic_reader(text:str):
-    
-    def bold_word(word, idx):
-        bold = '\033[1m'
-        end = '\033[0m'
-        return f"{bold}{word[:idx]}{end}{word[idx:]}"
+def bionic_reader(text: str, text_type: str) -> str:
+    def bold_word(word, idx, text_type) -> str:
+        ansi_bold = "\033[1m"
+        ansi_end = "\033[0m"
+        markdown_bold = "**"
+        if text_type == "ansi":
+            return f"{ansi_bold}{word[:idx]}{ansi_end}{word[idx:]}"
+        elif text_type == "markdown":
+            return f"{markdown_bold}{word[:idx]}{markdown_bold}{word[idx:]}"
+
     bolded_text = []
     for word in text.split():
-        word_split_idx = floor(len(word)/2)
-        if len(word) ==3:
-            word_split_idx=2
-        word = bold_word(word,word_split_idx)
+        word_split_idx = floor(len(word) / 2)
+        if len(word) == 3:
+            word_split_idx = 2
+        word = bold_word(word, word_split_idx, text_type)
         bolded_text.append(word)
     text = " ".join(bolded_text)
     return text
