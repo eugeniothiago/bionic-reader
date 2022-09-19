8ff3c6cd4d687d8ada293d0791a72d443c5a932c
eugeniothiago <thiagoace1@hotmail.com>
2022-09-18 23:45:18 -0300

    changed parameter name

--- bionic_reader.py
+++ bionic_reader.py
@@ -2,18 +2,18 @@ import os
 from math import floor
 
 
-def bionic_reader(text: str, text_type: str) -> str:
-    def bold_word(word, idx, text_type) -> str:
+def bionic_reader(text: str, bold_bold_output_type: str) -> str:
+    def bold_word(word, idx, bold_output_type) -> str:
         ansi_bold = "\033[1m"
         ansi_end = "\033[0m"
         markdown_bold = "**"
         html_bold = "<b>"
         html_bold_end = "</b>"
-        if text_type == "ansi":
+        if bold_output_type == "ansi":
             return f"{ansi_bold}{word[:idx]}{ansi_end}{word[idx:]}"
-        elif text_type == "markdown":
+        elif bold_output_type == "markdown":
             return f"{markdown_bold}{word[:idx]}{markdown_bold}{word[idx:]}"
-        elif text_type == 'html':
+        elif bold_output_type == 'html':
             return f"{html_bold}{word[:idx]}{html_bold_end}{word[idx:]}"
 
     bolded_text = []
@@ -21,7 +21,7 @@ def bionic_reader(text: str, text_type: str) -> str:
         word_split_idx = floor(len(word) / 2)
         if len(word) == 3:
             word_split_idx = 2
-        word = bold_word(word, word_split_idx, text_type)
+        word = bold_word(word, word_split_idx, bold_output_type)
         bolded_text.append(word)
     text = " ".join(bolded_text)
     return text
